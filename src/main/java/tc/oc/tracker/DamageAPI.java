f2a45f869d4c053c498fd55a2fa8c457dcb373fd
Steve Anton <anxuiz.nx@gmail.com>
2012-12-27 09:50:07 -0800

    Add inflectDamage API call.

--- src/main/java/tc/oc/tracker/DamageAPI.java
+++ src/main/java/tc/oc/tracker/DamageAPI.java
@@ -1,14 +1,105 @@
 package tc.oc.tracker;
 
-import javax.annotation.Nonnull;
+import java.util.HashMap;
+import java.util.Map;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import org.bukkit.Bukkit;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
+
+import tc.oc.tracker.resolvers.DamageAPIResolver;
 
 import com.google.common.base.Preconditions;
 
+/**
+ * Provides convenient static API calls for other plugins to use.
+ */
 public final class DamageAPI {
     private DamageAPI() { }
+
+    /**
+     * Calls {@link #inflictDamage} with force off.
+     */
+    public static boolean inflictDamage(@Nonnull LivingEntity entity, @Nonnull Damage damage) {
+        return inflictDamage(entity, damage, false);
+    }
+
+    /**
+     * Inflicts the given damage on an entity.
+     *
+     * This method will call the appropriate damage method and fire an {@link EntityDamageEvent}.
+     *
+     * @param entity Entity to inflict damage upon
+     * @param damage Damage object that describes this bit of damage
+     * @param force Indicates whether this method should respect cancellations
+     * @return
+     */
+    public static boolean inflictDamage(@Nonnull LivingEntity entity, @Nonnull Damage damage, boolean force) {
+        Preconditions.checkNotNull(entity, "living entity");
+        Preconditions.checkNotNull(damage, "damage");
+
+        EntityDamageEvent event = new EntityDamageEvent(entity, DamageCause.CUSTOM, damage.getHearts());
+        setAPIDamage(event, damage);
+
+        Bukkit.getPluginManager().callEvent(event);
+
+        if(event.isCancelled() && !force) {
+            return false;
+        }
+
+        entity.damage(damage.getHearts());
+
+        setAPIDamage(event, null);
+
+        return true;
+    }
+
+    private static @Nonnull Map<EntityDamageEvent, Damage> apiDamageMapping = new HashMap<EntityDamageEvent, Damage>();
+
+    /**
+     * Fetches the given {@link Damage} object specified by
+     * {@link #inflictDamage} so it can be fetched by the
+     * {@link DamageAPIResolver} when plugins who listen to the Bukkit event
+     * ask for it.
+     *
+     * Intended for internal use only, so this method is not guaranteed to stay
+     * the same through stable version releases.
+     *
+     * @param event EntityDamageEvent to fetch the given damage for
+     * @return Stored damage instance of null if none is stored
+     *
+     * @throws NullPointerException if event is null
+     */
+    public static @Nullable Damage getAPIDamage(@Nonnull EntityDamageEvent event) {
+        Preconditions.checkNotNull(event, "entity damage event");
+
+        return apiDamageMapping.get(event);
+    }
+
+    /**
+     * Sets the API damage that corresponds to the specified event.
+     *
+     * Intended for internal use only, so this method is not guaranteed to stay
+     * the same through stable version releases.
+     *
+     * @param event Specified event
+     * @param damage Damage object that represents the damage for the event
+     *
+     * @throws NullPointerException if event is null
+     */
+    public static void setAPIDamage(@Nonnull EntityDamageEvent event, @Nullable Damage damage) {
+        Preconditions.checkNotNull(event, "entity damage event");
+
+        if(damage != null) {
+            apiDamageMapping.put(event, damage);
+        } else {
+            apiDamageMapping.remove(event);
+        }
+    }
 
     /**
      * Gets the {@link Damage} instance that corresponds to the specified event.
---
+++ src/main/java/tc/oc/tracker/resolvers/DamageAPIResolver.java
@@ -0,0 +1,25 @@
+package tc.oc.tracker.resolvers;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import org.bukkit.entity.Entity;
+import org.bukkit.event.entity.EntityDamageEvent;
+
+import tc.oc.tracker.*;
+
+/**
+ * Resolves the damage stored in the {@link DamageAPI}.
+ *
+ * When a plugin uses the API to inflict damage on an entity, it specifies its
+ * own {@link Damage} object to use. However, plugins listening on the Bukkit
+ * event will use the regular channels to fetch the object for the event.
+ * Therefore, this resolver is necessary to feed the proper object to those
+ * event listeners.
+ */
+public class DamageAPIResolver implements DamageResolver {
+    /** @see DamageResolvers#resolve */
+    public @Nullable Damage resolve(@Nonnull Entity entity, @Nonnull Lifetime lifetime, @Nonnull EntityDamageEvent damageEvent) {
+        return DamageAPI.getDamage(damageEvent);
+    }
+}
