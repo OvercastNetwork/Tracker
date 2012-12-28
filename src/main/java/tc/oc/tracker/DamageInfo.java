e7cf83fb5368256dfd816ad0e71144dcdcf1e8b5
Steve Anton <anxuiz.nx@gmail.com>
2012-12-28 11:55:06 -0800

    Add some documentation to DamageInfo.

--- src/main/java/tc/oc/tracker/DamageInfo.java
+++ src/main/java/tc/oc/tracker/DamageInfo.java
@@ -4,6 +4,16 @@ import javax.annotation.Nullable;
 
 import org.bukkit.entity.LivingEntity;
 
+/**
+ * Provides more detailed information about a damage instance.
+ *
+ * Subclasses should be completely immutable.
+ */
 public interface DamageInfo {
+    /**
+     * Gets the living entity most responsible for this damage.
+     *
+     * @return Resolved damager or null if none exists
+     */
     @Nullable LivingEntity getResolvedDamager();
 }
