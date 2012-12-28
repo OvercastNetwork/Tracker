eaa25eb842a307e83c75d9f8f45f15b6fbb111d5
Steve Anton <anxuiz.nx@gmail.com>
2012-12-28 12:00:37 -0800

    Add more information to EntityDamageEvent.

--- src/main/java/tc/oc/tracker/event/EntityDamageEvent.java
+++ src/main/java/tc/oc/tracker/event/EntityDamageEvent.java
@@ -2,12 +2,14 @@ package tc.oc.tracker.event;
 
 import javax.annotation.Nonnull;
 
+import org.bukkit.Location;
 import org.bukkit.entity.Entity;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.entity.EntityEvent;
+import org.joda.time.Instant;
 
-import tc.oc.tracker.Damage;
+import tc.oc.tracker.DamageInfo;
 import tc.oc.tracker.Lifetime;
 
 import com.google.common.base.Preconditions;
@@ -17,27 +19,26 @@ import com.google.common.base.Preconditions;
  */
 public class EntityDamageEvent extends EntityEvent implements Cancellable {
     private final @Nonnull Lifetime lifetime;
-    private final @Nonnull Damage damage;
     private int hearts;
+    private final @Nonnull Location location;
+    private final @Nonnull Instant time;
+    private final @Nonnull DamageInfo info;
     private boolean cancelled = false;
 
-    public EntityDamageEvent(@Nonnull Entity entity, @Nonnull Lifetime lifetime, @Nonnull Damage damage) {
+    public EntityDamageEvent(@Nonnull Entity entity, @Nonnull Lifetime lifetime, int hearts, @Nonnull Location location, @Nonnull Instant time, @Nonnull DamageInfo info) {
         super(entity);
 
         Preconditions.checkNotNull(lifetime, "lifetime");
-        Preconditions.checkNotNull(damage, "damage");
+        Preconditions.checkArgument(hearts >= 0, "hearts must be greater than or equal to zero");
+        Preconditions.checkNotNull(location, "location");
+        Preconditions.checkNotNull(time, "time");
+        Preconditions.checkNotNull(info, "damage info");
 
         this.lifetime = lifetime;
-        this.damage = damage;
-        this.hearts = damage.getHearts();
-    }
-
-    public @Nonnull Lifetime getLifetime() {
-        return this.lifetime;
-    }
-
-    public @Nonnull Damage getDamage() {
-        return this.damage;
+        this.hearts = hearts;
+        this.location = location.clone();
+        this.time = time;
+        this.info = info;
     }
 
     public int getHearts() {
@@ -48,6 +49,22 @@ public class EntityDamageEvent extends EntityEvent implements Cancellable {
         Preconditions.checkArgument(hearts >= 0, "hearts must be greater than or equal to zero");
 
         this.hearts = hearts;
+    }
+
+    public @Nonnull Location getLocation() {
+        return this.location;
+    }
+
+    public @Nonnull Instant getTime() {
+        return this.time;
+    }
+
+    public @Nonnull Lifetime getLifetime() {
+        return this.lifetime;
+    }
+
+    public @Nonnull DamageInfo getInfo() {
+        return this.info;
     }
 
     public boolean isCancelled() {
