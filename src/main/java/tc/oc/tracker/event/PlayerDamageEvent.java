96e34a676cd26eb766b62b02372805e00ef372d5
thekeenant <keenan@keenant.com>
2016-02-06 17:36:04 -0600

    Add handlers for PlayerDamageEvent

--- src/main/java/tc/oc/tracker/event/PlayerDamageEvent.java
+++ src/main/java/tc/oc/tracker/event/PlayerDamageEvent.java
@@ -3,6 +3,7 @@ package tc.oc.tracker.event;
 import org.bukkit.Location;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
+import org.bukkit.event.HandlerList;
 import org.joda.time.Instant;
 import tc.oc.tracker.DamageInfo;
 import tc.oc.tracker.Lifetime;
@@ -12,5 +13,14 @@ import javax.annotation.Nonnull;
 public class PlayerDamageEvent extends EntityDamageEvent<Player> {
     public PlayerDamageEvent(@Nonnull Player player, @Nonnull Lifetime lifetime, int damage, @Nonnull Location location, @Nonnull Instant time, @Nonnull DamageInfo info) {
         super(player, lifetime, damage, location, time, info);
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
     }
 }
